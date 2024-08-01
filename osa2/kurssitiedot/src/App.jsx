074e1d13d6b48bec6568564f76008b06b465e5ba
Kerkko Pelttari <kerk.pelt@gmail.com>
2024-08-01 13:18:09 +0300

    Complete 2.5

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,36 +1,4 @@
-const Header = ({course}) => (
-    <h1>{course}</h1>
-)
-
-const Total = ({parts}) => {
-  console.log(parts)
-  const amount = parts.reduce((accum, current) => accum + current.exercises, 0)
-  return <p>Number of exercises {amount}</p>
-}
-
-const Content = ({ parts }) => {
-  return <div>
-    {parts.map(element => {
-        console.log("test");
-        return <Part part={element} key={element.name} />
-    })}
-  </div>
-
-}
-const Part = ({part}) => {
-  return <p>
-    {part.name} {part.exercises}
-  </p>
-}
-
-const Course = ({course}) => {
-    return <div>
-        <Header course={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-    </div>
-
-}
+import Course from "./Course"
 
 const App = () => {
     const courses = [
