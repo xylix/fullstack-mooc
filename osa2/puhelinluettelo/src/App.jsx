ef2f29b2e55b98cb175edcee878ca5c7eb0d8aa0
Kerkko Pelttari <kerk.pelt@gmail.com>
2024-08-02 12:38:17 +0300

    Complete 2.9 and 2.10

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,11 +1,34 @@
 import { useState } from 'react'
 
+const Filter = ({onChange}) => (
+ <div>filter shown with <input onChange={onChange}/></div>
+)
+
+const PersonForm = ({ onSubmit, onNameChange, onNumberChange}) => (
+  <form onSubmit={onSubmit}>
+    <div>
+      name: <input onChange={onNameChange}/>
+      number: <input onChange={onNumberChange}/>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
+const Persons = ({personsToShow}) => (
+  personsToShow.map(person => (
+    <div key={person.name}>{person.name} {person.number}</div>
+  ))
+)
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const submit = (event) => {
     event.preventDefault()
@@ -27,21 +50,20 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+  const personsToShow = persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={submit}>
-        <div>
-          name: <input onChange={handleNameChange}/>
-          number: <input onChange={handleNumberChange}/>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter onChange={handleFilterChange}/>
+      <h4>Add a new</h4>
+      <PersonForm onSubmit={submit} onNameChange={handleNameChange} onNumberChange={handleNumberChange}/>
+
       <h2>Numbers</h2>
-        {persons.map(person => (
-            <div key={person.name}>{person.name} {person.number}</div>
-        ))}
+      <Persons personsToShow={personsToShow} />
       {/* <div>debug: {newName}</div> */}
     </div>
   )
