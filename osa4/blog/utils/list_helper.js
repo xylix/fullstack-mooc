f6d0dd213bd50f0afe8cf96ae4f9e4a8c26f31cf
Kerkko Pelttari <kerk.pelt@gmail.com>
2024-08-27 14:07:47 +0300

    4.4, 4.5

--- osa4/blog/utils/list_helper.js
+++ osa4/blog/utils/list_helper.js
@@ -2,6 +2,23 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((accum, blog) => accum + blog.likes, 0)
+}
+
+const favouriteBlog = (blogs) => {
+  max = -1
+  index = -1
+  blogs.forEach((blog, i) => {
+    if (blog.likes > max) {
+      max = blog.likes;
+      index = i;
+    }
+  });
+  return blogs[index]
+}
+
+
 module.exports = {
-  dummy
+  dummy, totalLikes, favouriteBlog
 }
