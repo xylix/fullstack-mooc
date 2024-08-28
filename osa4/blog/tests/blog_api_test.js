4142e795893f8f49e4b8a0d30e6bc0773d93b13d
Kerkko Pelttari <kerk.pelt@gmail.com>
2024-08-28 11:29:08 +0300

    4.8

---
+++ osa4/blog/tests/blog_api_test.js
@@ -0,0 +1,32 @@
+const { test, after } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('correct amount of blogs is returned', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+    .expect(response => {
+      if (response.body.length !== 3) {
+        throw new Error('incorrect amount of blogs')
+      }
+    })
+})
+
+test('blog id is named `id`', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(response => {
+      if (!response.body[0].id) {
+        throw new Error(`id is not named 'id', response had fields ${JSON.stringify(Object.values(response.body[0]))}`)
+      }
+    })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
